6e0de228b08f246880ad35ac697ab89b597123f7
aike <aaiikkee@gmail.com>
2018-09-13 13:21:28 +0900

    adjust param

--- voice.js
+++ voice.js
@@ -102,7 +102,7 @@ class Voice
         this.osc.connect(this.gain);
         this.gain.connect(audioctx.destination);
         this.level = 0.03;
-        this.eg_t=[0, 0.05, 0.20, 0.3];
+        this.eg_t=[0, 0.05, 0.20, 1.5];
         this.eg_a=[0, 0,    1,    1];
         break;
       case "sy":
@@ -117,7 +117,7 @@ class Voice
         this.consoFilter.connect(this.gain);
         this.gain.connect(audioctx.destination);
         this.level = 0.03;
-        this.eg_t=[0, 0.05, 0.20, 0.3];
+        this.eg_t=[0, 0.05, 0.20, 1.5];
         this.eg_a=[0, 0,    1,    1];
         break;
       case "t":
